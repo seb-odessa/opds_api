5b7eddd9f579841da39293b08e4427ede8010347
seb <seb@ukr.net>
2024-07-11 17:21:53 +0300

    Added trivial test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// use anyhow::anyhow;
+use log::{debug, error};
+use rusqlite::Connection;
+
+use std::convert::TryFrom;
+
+#[derive(Debug)]
+pub enum QueryType {
+    Author,
+    Serie,
+}
+
+#[derive(Debug)]
+pub struct OpdsApi {
+    conn: Connection,
+}
+impl TryFrom<&str> for OpdsApi {
+    type Error = anyhow::Error;
+
+    fn try_from(database: &str) -> anyhow::Result<Self> {
+        debug!("database: {database}");
+        let conn = Connection::open(&database).inspect_err(|e| error!("{e}"))?;
+        Ok(Self { conn })
+    }
+}
+impl TryFrom<&String> for OpdsApi {
+    type Error = anyhow::Error;
+
+    fn try_from(database: &String) -> anyhow::Result<Self> {
+        debug!("database: {database}");
+        OpdsApi::try_from(database.as_str())
+    }
+}
+
+impl OpdsApi {
+    pub fn is_readonly(&self) -> anyhow::Result<bool> {
+        Ok(self.conn.is_readonly(rusqlite::DatabaseName::Main)?)
+    }
+
+
 }
 
 #[cfg(test)]
@@ -7,8 +45,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn new() -> anyhow::Result<()>{
+        let api = OpdsApi::try_from("file:/lib.rus.ec/books.db?mode=ro")?;
+        assert!(api.is_readonly()?);
+        Ok(())
     }
 }
